950eb3a3e6fa68ec4000a1d65480d93e760fc8c4
apple <apple@apples-MacBook-Air-3.local>
2026-05-03 00:53:44 +0530

    fixed input ranges

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ with st.container():
     # LEFT SIDE
     with col1:
         preg = st.number_input("Pregnancies", min_value=0)
-        glucose = st.number_input("Glucose", min_value=0)
+        glucose = st.number_input("Glucose", min_value=70, max_value=200, value=100)
         skin = st.number_input("Skin Thickness", min_value=0)
         dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0)
 
 
     # RIGHT SIDE
     with col2:
-        bp = st.number_input("Blood Pressure", min_value=0)
-        insulin = st.number_input("Insulin", min_value=0)
-        bmi = st.number_input("BMI", min_value=0.0)
-        age = st.number_input("Age", min_value=1)
+        bp = st.number_input("Blood Pressure", min_value=40, max_value=200, value=80)
+        insulin = st.number_input("Insulin", min_value=15, max_value=300, value=80)
+        bmi = st.number_input("BMI", min_value=10.0, max_value=50.0, value=25.0)
+        age = st.number_input("Age", min_value=1, max_value=120, value=25)
 
 # Button outside container (clean UI)
 if st.button("🔍 Predict", use_container_width=True):
